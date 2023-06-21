3a3ff3e7e1f0fd415dbe4efb9e0dd09427aede07
ENDALE LEGESSE <101863853+Endale24@users.noreply.github.com>
2023-06-21 22:03:51 +0800

    web app dev

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
-const response = await fetch('/upload', {
-  method: 'POST',
-  body: formData,
-});
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+import Login from "./components/Login";
+import Home from "./container/Home";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
+}
+
+export default App;
